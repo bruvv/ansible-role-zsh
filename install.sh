5b42bf597dc22549c9b5649de0b8dbe99260ed36
Niels <niels.voorn@robinradar.com>
2022-11-16 14:44:47 +0100

    do not install old role

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ sudo apt update
 sudo apt install python3-pip -y
 sudo pip3 install ansible
 
-title "Install viasite-ansible.zsh"
-ansible-galaxy install viasite-ansible.zsh --force
+# title "Install viasite-ansible.zsh"
+# ansible-galaxy install viasite-ansible.zsh --force
 
 title "Download playbook to /tmp/zsh.yml"
 curl https://raw.githubusercontent.com/bruvv/ansible-role-zsh/master/playbook.yml >/tmp/zsh.yml
